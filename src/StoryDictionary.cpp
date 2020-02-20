d92d49d501c6b211bf609165cf389cc1563d1c76
sm1f <stephen_morrisson@yahoo.com>
2020-02-20 18:11:55 -0500

    Started thinking about StoryDictionary.

---
+++ src/StoryDictionary.cpp
@@ -0,0 +1,16 @@
+// CopyRight Stephen Morrisson 2020
+// All rights reserved.
+
+
+#include "StoryDictionary.h"
+
+StoryDictionary::StoryDictionary()
+  : CppBase()
+{
+}
+
+StoryDictionary::~StoryDictionary()
+{
+}
+
+
---
+++ src/StoryDictionary.h
@@ -0,0 +1,21 @@
+// CopyRight Stephen Morrisson 2020
+// All rights reserved.
+
+
+#ifndef  __STORY_DICTIONARY_H__
+#define __STORY_DICTIONARY_H__
+
+#include "CppBase.h"
+
+class StoryDictionary : public CppBase
+{
+ public:
+  StoryDictionary();
+  virtual ~StoryDictionary();
+ private:
+  typedef CppBase __super;
+
+};
+
+#endif //  __STORY_DICTIONARY_H__
+
