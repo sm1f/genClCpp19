4ead92ee56c646111cafe79696100e369c6ad84a
sm1f <stephen_morrisson@yahoo.com>
2020-03-18 19:17:03 -0400

    Checkpoint.  map stuff.

--- src/ClassicStateMachine.cpp
+++ src/ClassicStateMachine.cpp
@@ -3,6 +3,7 @@
 
 
 #include "ClassicStateMachine.h"
+#include <map>
 
 template<typename InputEnum, typename StateEnum, typename OutputEnum>
 class ClassicStateMachineTransition {
@@ -18,28 +19,80 @@ class ClassicStateMachineTransition {
   OutputEnum transOutputValue;
 };
 
+template<typename StateEnum, typename OutputEnum>
+class ClassicStateMachineOutput {
+ public:
+  StateEnum transToState;
+  OutputEnum transOutputValue;
+};
+
+template<typename KeyType, typename ValueType>
+class MyMap
+{
+ public:
+  typedef std::map<KeyType, ValueType> _BaseMapType;
+  _BaseMapType baseMap;
+  bool tryGetValue(KeyType key, ValueType& result2) {
+    _BaseMapType::iterator it = baseMap.find(key);
+    if (it == baseMap.end()) {
+      return false;
+    } else {
+      result2 = it->second;
+      return true;
+    }
+  }
+};
+
+
 template<typename InputEnum, typename StateEnum, typename OutputEnum>
 class ClassicStateMachine {
  public:
+  typedef ClassicStateMachineTransition<InputEnum, StateEnum, OutputEnum> _Transition;
+  typedef ClassicStateMachineOutput<StateEnum, OutputEnum> _OutputPair;
+  typedef map<InputEnum, _OutputPair> Input2OutMap;
+  typedef map<StateEnum, Input2OutMap> State2TransMap;
+  
+ private:
+  State2TransMap transitionMap;
+
+  //  DECLARE_MAP_GETVALUE(State2TransMap, StateEnum, Input2OutMap);
+  //  IMPL_MAP_GETVALUE(Input2OutMap, InputEnum, _OutputPair);
+  //  IMPL_MAP_GETVALUE(State2TransMap, StateEnum, Input2OutMap);
+
+ public:
   ClassicStateMachine() {}
 
-  void AddTransition(StateEnum fromState, InputEnum inputValue, StateEnum toState, OutputEnum outputValue)
-  {
-  }
+  void foo() {}
   
+  _Transition* addTransition(StateEnum fromState, InputEnum inputValue, StateEnum toState, OutputEnum outputValue)
+  {
+    //    if (transitionmap.try
+    
+    //_OutputPair outPair = _OutputPair(toState, outputValue);
+    
+    
+    _Transition* result = new _Transition(fromState, inputValue, toState, outputValue);
+    return result;
+  };
+
 };
 
 enum class MyState { State1, State2, State3 };
 enum class MyInput { In1, In2, In3 };
 enum class MyOutput { Out1, Out2, Out3 };
 
-class ClassicStateMachineTest : ClassicStateMachine<MyInput, MyState, MyOutput>
+class ClassicStateMachineTest : public ClassicStateMachine<MyInput, MyState, MyOutput>
 {
-  
+ public:
+  ClassicStateMachineTest() {}
+  void bar() {}
 };
 
 ClassicStateMachineUtility::ClassicStateMachineUtility()
   : CppBase()
 {
+  ClassicStateMachineTest* machine = new ClassicStateMachineTest();
+  //  machine->bar();
+    machine->addTransition(MyState::State1, MyInput::In1, MyState::State2, MyOutput::Out1);
 }
 
